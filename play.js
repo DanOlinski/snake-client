2ee6b52d3e7a096a0235e4977c8ff16586f76afc
DanOlinski <daniel.guterres@hotmail.com>
2023-02-20 16:42:45 -0700

    modulate functions from play.js into client.js

--- play.js
+++ play.js
@@ -1,25 +1,5 @@
-const net = require("net");
-
 //this function handles different aspects of the network connection to the server where the game Snek runs
-const connect = function() {
+const connect = require("./client")
 
-  //this function connects to a server through an IP address and specified port
-  const conn = net.createConnection({
-    // the keyword local host only works if you are connecting with yout own computer as a test. To connect to another computer you need to replace the keyword with the IP you want to connect to
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-
-  //this function get's anny data that the server sends to client and logs it in the clients terminal.
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-
-};
-
-console.log('connecting ...');
+console.log('Connecting ...');
 connect();
